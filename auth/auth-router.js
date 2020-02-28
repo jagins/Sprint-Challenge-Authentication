75871d7d04c62dea6f9318de7acfcc1a8f84d9c1
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-28 10:22:39 -0600

    register works

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+const Users = require('../users/user-model');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+
+  if(!user.username && !user.password)
+  {
+    res.status(400).json({message: 'please provide a username and password'});
+  }
+  else
+  {
+    const hashPassword = bcrypt.hashSync(user.password, 8);
+
+    user.password = hashPassword;
+
+    Users.addUser(user)
+    .then(createdUser =>
+    {
+      res.status(200).json(createdUser);
+    })
+    .catch(error =>
+    {
+      res.status(500).json({error: 'could not save to the database'});
+    })
+  }
 });
 
 router.post('/login', (req, res) => {
