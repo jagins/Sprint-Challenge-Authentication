2162ce7bb222ccca346cc7b9d86acb07601db865
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-28 10:32:18 -0600

    login works and token is provided

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/user-model');
 
@@ -29,7 +30,35 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
-});
+  const {username, password} = req.body;
+
+  Users.findUser({username}).first()
+  .then(user =>
+  {
+    if(user && bcrypt.compareSync(password, user.password))
+    {
+      const token = createToken(user)
+
+      res.status(200).json({message: `Welcome ${user.username}`, token});
+    }
+    else
+    {
+      res.status(400).json({error: 'username or password is invalid'});
+    }
+  })
+})
+
+function createToken(user)
+{
+  const payload = {
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: '1hr'
+  }
+
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
 
 module.exports = router;
